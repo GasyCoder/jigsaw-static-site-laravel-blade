a62583971591aaadd296ed31c49fc129cac8d05b
gasycoder <florent@gasycoder.com>
2022-03-16 20:37:21 +0300

    my first jigsaw-commit

---
+++ source/_nav/menu-responsive.blade.php
@@ -0,0 +1,32 @@
+<nav id="js-nav-menu" class="w-auto px-2 pt-6 pb-2 bg-gray-200 shadow hidden lg:hidden">
+    <ul class="my-0">
+        <li class="pl-4">
+            <a
+                title="{{ $page->siteName }} Blog"
+                href="/blog"
+                class="block mt-0 mb-4 text-sm no-underline {{ $page->isActive('/blog') ? 'active text-blue-500' : 'text-gray-800 hover:text-blue-500' }}"
+            >Blog</a>
+        </li>
+         <li class="pl-4">
+            <a
+                title="{{ $page->GasyCoder }} GasyCoder"
+                href="https://gasycoder.com" target="_blank"
+                class="block mt-0 mb-4 text-sm no-underline {{ $page->isActive('https://gasycoder.com') ? 'active text-blue-500' : 'text-gray-800 hover:text-blue-500' }}"
+            >Company</a>
+        </li>
+        <li class="pl-4">
+            <a
+                title="{{ $page->siteName }} About"
+                href="/about"
+                class="block mt-0 mb-4 text-sm no-underline {{ $page->isActive('/about') ? 'active text-blue-500' : 'text-gray-800 hover:text-blue-500' }}"
+            >About</a>
+        </li>
+        <li class="pl-4">
+            <a
+                title="{{ $page->siteName }} Contact"
+                href="/contact"
+                class="block mt-0 mb-4 text-sm no-underline {{ $page->isActive('/contact') ? 'active text-blue-500' : 'text-gray-800 hover:text-blue-500' }}"
+            >Contact</a>
+        </li>
+    </ul>
+</nav>
---
+++ source/_posts/hello-world.md
@@ -0,0 +1,13 @@
+---
+extends: _layouts.post  
+section: content
+title: Hello World
+date: 2022-03-15
+author: Florent
+description: Hello World and Jigsaw
+cover_image: /assets/img/post-cover-image-2.png
+alt: hello world
+categories: [configuration]
+---
+
+Hello Gasy Coder
